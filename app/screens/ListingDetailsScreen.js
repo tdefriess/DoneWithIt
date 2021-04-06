87bbd1ff34646f06489c7ecd5823fa809e8e0f07
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-06 15:52:53 -0400

    Implement ListItem component, improved ListingDetailsScreen

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, Image } from 'react-native';
 import AppText from '../components/AppText';
+import ListItem from '../components/ListItem';
 import colors from '../config/colors';
 
 function ListingDetailsScreen(props) {
@@ -10,6 +11,13 @@ function ListingDetailsScreen(props) {
             <View style={styles.detailsContainer}>
                 <AppText style={styles.title}>Red jacket for sale!</AppText>
                 <AppText style={styles.price}>$100</AppText>
+                <View style={styles.userContainer}>
+                <ListItem 
+                    image={require('../assets/tyler.jpg')}
+                    title='Tyler de Friess'
+                    subTitle='5 Listings'
+                />
+                </View>
             </View>
         </View>
     );
@@ -32,6 +40,9 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         fontWeight: '500'
+    },
+    userContainer: {
+        marginVertical: 40
     }
 })
 
