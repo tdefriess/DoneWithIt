769f3d116932a4d9aeae2aac3b9432d9c79a2d67
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-06 17:28:50 -0400

    Implemented ListItemDeleteAction

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -4,6 +4,7 @@ import { FlatList, StyleSheet, View } from 'react-native';
 import Screen from '../components/Screen';
 import ListItem from '../components/ListItem';
 import ListItemSeparator from '../components/ListItemSeparator';
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
 
 const messages = [
     {
@@ -31,6 +32,8 @@ function MessagesScreen(props) {
                     title={item.title}
                     subTitle={item.description}
                     image={item.image}
+                    onPress={() => console.log('message selected', item)}
+                    renderRightActions={ListItemDeleteAction}
                 />}
                 ItemSeparatorComponent={ListItemSeparator}
             />
