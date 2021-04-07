2b3533949cec7f16f8441a88773a2ab5d88faab2
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-07 10:53:54 -0400

    Implemented ListingScreen

--- App.js
+++ App.js
@@ -13,10 +13,11 @@ import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import MyAccountScreen from './app/screens/MyAccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 export default function App() {
   return (
-    <MyAccountScreen />
+    <ListingsScreen />
   );
 }
 
