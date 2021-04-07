070892f11891745cedb6822f364b80a231bb27d1
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-07 10:18:12 -0400

    Implemented icon component.

--- App.js
+++ App.js
@@ -9,10 +9,19 @@ import AppButton from './app/components/AppButton';
 import AppCard from './app/components/AppCard';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
   return (
-    <MessagesScreen />
+    <Screen>
+      <Icon
+        name="email"
+        size={50}
+        backgroundColor="red"
+        iconColor="white"
+      />
+    </Screen>
   );
 }
 
