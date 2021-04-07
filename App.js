44dcd79f0062d7f7c7420175ace6eb02a8ea65e1
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-07 10:45:42 -0400

    Implemented MyAccountScreen

--- App.js
+++ App.js
@@ -12,16 +12,11 @@ import MessagesScreen from './app/screens/MessagesScreen';
 import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
+import MyAccountScreen from './app/screens/MyAccountScreen';
 
 export default function App() {
   return (
-    <Screen>
-      <ListItem 
-        title='My title' 
-        // subTitle="My subtitle" 
-        ImageComponent={<Icon name="email" />}  
-      />
-    </Screen>
+    <MyAccountScreen />
   );
 }
 
