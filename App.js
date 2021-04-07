a4db32256860276710547539bd7d9f21f49feb93
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-07 15:19:36 -0400

    Added RegisterScreen.

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 
 import Screen from './app/components/Screen';
 import LoginScreen from './app/screens/LoginScreen';
+import RegisterScreen from './app/screens/RegisterScreen';
 
 export default function App() {
 
   return (
-    <LoginScreen />
+    <RegisterScreen />
   );
 }
