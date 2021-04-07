882ed28b0e84b34419d15e14e77dd6a0665f8d08
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-07 14:04:51 -0400

    Started building login screen.

--- App.js
+++ App.js
@@ -1,40 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Switch } from 'react-native';
-import AppPicker from './app/components/AppPicker';
-import AppTextInput from './app/components/AppTextInput';
 
 import Screen from './app/components/Screen';
-
-categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 },
-]
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
 
   return (
-    <Screen>
-      <AppPicker 
-        items={categories} 
-        icon="apps" 
-        placeholder="Category" 
-        selectedItem={category}
-        onSelectItem={item => setCategory(item)}
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
+    <LoginScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
-  },
-});
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { StyleSheet, Image } from 'react-native';
+import { Formik } from 'formik'
+import * as Yup from 'yup';
+
+import AppButton from '../components/AppButton';
+import AppTextInput from '../components/AppTextInput';
+import Screen from '../components/Screen';
+import AppText from '../components/AppText';
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(4).label("Password")
+})
+
+function LoginScreen(props) {
+
+    return (
+        <Screen style={styles.container}>
+            <Image 
+                style={styles.logo}
+                source={require("../assets/logo-red.png")}
+            />
+
+            <Formik
+                initialValues={{ email: '', password: ''}}
+                onSubmit={values => console.log(values)}
+                validationSchema={validationSchema}
+            >
+                { ({ handleChange, handleSubmit, errors }) => (
+                    <>
+                        <AppTextInput 
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            icon="email"
+                            keyboardType="email-address"
+                            name="email"
+                            onChange={handleChange("email")}
+                            placeholder="Email"
+                            textContentType="emailAddress"
+                        />
+                        <AppText style={{color: 'red'}}>{errors.email}</AppText>
+                        <AppTextInput 
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            icon="lock"
+                            name="password"
+                            onChange={handleChange("password")}
+                            placeholder="Password"
+                            secureTextEntry
+                            textContentType="password"
+                        />
+                        <AppButton title="Login" onPress={handleSubmit} />
+                        <AppText style={{color: 'red'}}>{errors.password}</AppText>
+
+                    </>
+                ) }
+            </Formik>
+
+
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10
+    },
+    logo: {
+        width: 80,
+        height: 80,
+        alignSelf: 'center',
+        marginTop: 50,
+        marginBottom: 20
+    }
+})
+
+export default LoginScreen;
