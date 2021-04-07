6d5688388678d6af635fdc730eb4b6595746634e
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-07 14:17:43 -0400

    Added visible prop to error component and set onBlur properties for inputs.

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -5,8 +5,8 @@ import * as Yup from 'yup';
 
 import AppButton from '../components/AppButton';
 import AppTextInput from '../components/AppTextInput';
+import ErrorMessage from '../components/ErrorMessage';
 import Screen from '../components/Screen';
-import AppText from '../components/AppText';
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -27,7 +27,7 @@ function LoginScreen(props) {
                 onSubmit={values => console.log(values)}
                 validationSchema={validationSchema}
             >
-                { ({ handleChange, handleSubmit, errors }) => (
+                { ({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
                     <>
                         <AppTextInput 
                             autoCapitalize="none"
@@ -35,23 +35,25 @@ function LoginScreen(props) {
                             icon="email"
                             keyboardType="email-address"
                             name="email"
-                            onChange={handleChange("email")}
+                            onBlur={() => setFieldTouched("email")}
+                            onChangeText={handleChange("email")}
                             placeholder="Email"
                             textContentType="emailAddress"
                         />
-                        <AppText style={{color: 'red'}}>{errors.email}</AppText>
+                        <ErrorMessage error={errors.email} visible={touched.email} />
                         <AppTextInput 
                             autoCapitalize="none"
                             autoCorrect={false}
                             icon="lock"
                             name="password"
-                            onChange={handleChange("password")}
+                            onBlur={() => setFieldTouched("password")}
+                            onChangeText={handleChange("password")}
                             placeholder="Password"
                             secureTextEntry
                             textContentType="password"
                         />
+                        <ErrorMessage error={errors.password} visible={touched.password} />
                         <AppButton title="Login" onPress={handleSubmit} />
-                        <AppText style={{color: 'red'}}>{errors.password}</AppText>
 
                     </>
                 ) }
