1f2609e56c08addf215f591144ef58d5bf61e973
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-08 18:00:02 -0400

    Built ImageInput component.

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 import Screen from './app/components/Screen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
@@ -6,10 +8,41 @@ import ListingsScreen from './app/screens/ListingsScreen';
 import LoginScreen from './app/screens/LoginScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
+import { Button, Image } from 'react-native';
+import ImageInput from './app/components/forms/ImageInput'
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted)
+      alert('You need to enable permission to access the library.')
+  }
+
+  useEffect(() => {
+    requestPermission();
+  }, [])
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled)
+        setImageUri(result.uri);
+    } catch (error) {
+      console.log('Error reading an image:', error);
+    }
+  }
 
   return (
-    <ListingEditScreen />
+    // <Screen>
+    //   <Button title="Select Image" onPress={selectImage} />
+    //   <Image source={{ uri: imageUri}} style={{ width: 200, height:}} />
+    // </Screen>
+    <ImageInput
+      imageUri={imageUri}
+      onChangeImage={(uri) => setImageUri(uri)}
+      onPress={selectImage}
+    />
   );
 }
---
+++ app/components/forms/ImageInput.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+import defaultStyles from '../../config/styles'
+import Icon from '../Icon';
+import Screen from '../Screen';
+
+function ImageInput({ imageUri, onChangeImage }) {
+    const selectImage = async () => {
+        try {
+            const result = await ImagePicker.launchImageLibraryAsync();
+            if (!result.cancelled)
+                onChangeImage(result.uri);
+        } catch (error) {
+            console.log('Error reading an image', error)
+        }
+    }
+
+    return (
+        <Screen>
+            <TouchableWithoutFeedback onPress={selectImage}>
+                <View style={styles.container}>
+                    { imageUri ?
+                        <Image 
+                            source={{ uri: imageUri }}
+                            style={styles.image}
+                        />
+                    :
+                        <Icon
+                            borderRadius={20}
+                            name="camera" 
+                            size={120} 
+                            backgroundColor={defaultStyles.colors.light}
+                            iconColor={defaultStyles.colors.medium}
+                        />
+                    }
+                </View>
+            </TouchableWithoutFeedback>
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        // height: 100,
+        // width: 100,
+        // backgroundColor: defaultStyles.colors.light,
+        // borderRadius: 20
+    },
+    image: {
+        height: 120,
+        width: 120,
+        borderRadius: 20
+    }
+})
+
+export default ImageInput;
