b90904730a7df52efc94453614e789a5f47f6832
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-10 16:17:38 -0400

    Set up api to get listings, connected to Listings Screen.

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 
 import AppCard from '../components/AppCard';
 import colors from '../config/colors';
+import listingsApi from '../api/listings';
 import routes from '../navigation/routes';
 import Screen from '../components/Screen';
 
-const listings = [
-    {
-        id: 1,
-        title: 'Red Jacket for sale',
-        price: 100,
-        image: require('../assets/jacket.jpg')
-    },
-    {
-        id: 2,
-        title: 'Couch in great condition',
-        price: 1000,
-        image: require('../assets/couch.jpg')
-    }
-]
 function ListingsScreen({ navigation }) {
+    const [listings, setListings] = useState([]);
+
+    useEffect(() => {
+        loadListings();
+    }, []);
+
+    const loadListings = async () => {
+        const response = await listingsApi.getListings();
+        setListings(response.data);
+    }
 
     return (
         <Screen style={styles.screen}>
@@ -31,7 +28,7 @@ function ListingsScreen({ navigation }) {
                     <AppCard
                         title={item.title}
                         subTitle={"$" + item.price}
-                        image={item.image}
+                        imageUrl={item.images[0].url}
                         onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
                     />
             }
