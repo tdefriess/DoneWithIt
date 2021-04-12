9bd39353c2fdc5c1114a8ae5d047ed2e507d09b0
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-12 14:56:50 -0400

    Added caching for listings.

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
 import AuthNavigator from './app/navigation/AuthNavigator';
 
 export default function App() {
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem('person', JSON.stringify({ id: 1}))
+      const value = await AsyncStorage.getItem('person');
+      const person = JSON.parse(value)
+      console.log(person);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  demo();
+
   return (
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
