3f7402d0313d8a464aea3bd19cffaa33ce3786cb
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-16 18:29:00 -0400

    Added offline notice.

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
-import AuthNavigator from './app/navigation/AuthNavigator';
+import OfflineNotice from './app/components/OfflineNotice';
 
 export default function App() {
-  const demo = async () => {
-    try {
-      await AsyncStorage.setItem('person', JSON.stringify({ id: 1}))
-      const value = await AsyncStorage.getItem('person');
-      const person = JSON.parse(value)
-      console.log(person);
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  demo();
+  
 
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
