d3be94851475f7202d2916ed930c384aaf27c88f
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-19 16:37:13 -0400

    Used React Context to capture user object, conditionally rendering AppNavigator when user object is captured and saved.

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { StyleSheet, Image } from 'react-native';
 import * as Yup from 'yup';
 import jwtDecode from 'jwt-decode';
@@ -6,6 +6,7 @@ import jwtDecode from 'jwt-decode';
 import Screen from '../components/Screen';
 import { AppForm, AppFormField, ErrorMessage, SubmitButton } from '../components/forms';
 import authApi from '../api/auth';
+import AuthContext from '../auth/context';
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -13,6 +14,7 @@ const validationSchema = Yup.object().shape({
 })
 
 function LoginScreen(props) {
+    const authContext = useContext(AuthContext);
     const [loginFailed, setLoginFailed] = useState(false);
 
     const handleSubmit = async ({ email, password }) => {
@@ -20,7 +22,7 @@ function LoginScreen(props) {
         if (!result.ok) return setLoginFailed(true);
         setLoginFailed(false);
         const user = jwtDecode(result.data);
-        console.log(user)
+        authContext.setUser(user);
     }
 
     return (
