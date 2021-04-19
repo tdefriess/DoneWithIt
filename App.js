2c9aaab36bc79cd09aa2de5a00ebd189ec6cfc4e
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-19 17:34:10 -0400

    Moved user token decoding logic to storage.js

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import jwtDecode from 'jwt-decode';
 import AppLoading from 'expo-app-loading';
 
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
 import OfflineNotice from './app/components/OfflineNotice';
 import AuthNavigator from './app/navigation/AuthNavigator';
-import { useState } from 'react';
 import AuthContext from './app/auth/context';
 import authStorage from './app/auth/storage';
 
@@ -15,14 +13,13 @@ export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken()
-    if (!token) return;
-    setUser(jwtDecode(token));
+  const restoreUser = async () => {
+    const user = await authStorage.getUser()
+    if (user) setUser(user);
   }
 
   if (!isReady)
-    return <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} onError={() => console.log(error)} />
+    return <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} onError={() => console.log(error)} />
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
