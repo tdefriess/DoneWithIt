d412c223c982f05ed69da0c1eb31794d8b08c2cf
Tyler de Friess <tyler.defriess@protonmail.com>
2021-04-21 23:12:21 -0400

    Added expoPushTokens.js to api and set up AppNavigator to get and register push tokens.

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
 
-import ListingEditScreen from '../screens/ListingEditScreen';
-import colors from '../config/colors';
-import FeedNavigator from './FeedNavigator';
 import AccountNavigator from './AccountNavigator';
+import colors from '../config/colors';
+import expoPushTokensApi from '../api/expoPushTokens';
+import FeedNavigator from './FeedNavigator';
+import ListingEditScreen from '../screens/ListingEditScreen';
 import NewListingButton from './NewListingButton';
 import routes from './routes';
-import { useEffect } from 'react';
 
 
 const Tab = createBottomTabNavigator();
@@ -21,11 +21,13 @@ const AppNavigator = () => {
 
   const registerForPushNotifications = async () => {
     try {
-      const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      // const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      const permission = await Notifications.getPermissionsAsync();
       if (!permission.granted) return;
   
       const token = await Notifications.getExpoPushTokenAsync();
-      console.log(token);
+      await expoPushTokensApi.register(token.data);
+      console.log(token.data);
     } catch (error) {
       console.log("Error getting a push token", error);
     }
